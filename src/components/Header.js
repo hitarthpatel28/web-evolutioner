3b8cb546aea1c45043f70b4a3c69ab65d7de5f87
hitarthpatel76 <hitarth.bece@gmail.com>
2025-03-24 00:48:18 -0400

    Logo Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import styled from 'styled-components';
+import Logo  from "../assets/images/logo-3.png";
 
 const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 20px;
-  background-color: #333;
-  color: #fff;
+  background-color: #fff;
+  color: #000;
+  align-items: center;
 `;
 
 const Header = () => (
   <HeaderContainer>
-    <div>Web Evolutioner</div>
+    <div><img src={Logo} width={300}/></div>
     <nav>
       <Link to="about" smooth={true} duration={500}>About</Link>
       <Link to="services" smooth={true} duration={500}>Services</Link>
